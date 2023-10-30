0fdb2dec592f9e31df21bc9097b762d898197716
ali <alisahin2607@gmail.com>
2023-10-30 22:07:05 +0300

    added "add" method for Product

--- src/main/java/org/example/dataAccess/ProductDaoJDBC.java
+++ src/main/java/org/example/dataAccess/ProductDaoJDBC.java
@@ -1,4 +1,10 @@
 package org.example.dataAccess;
 
+import org.example.entities.Product;
+
 public class ProductDaoJDBC {
+
+    public void add(Product product) {
+        System.out.println("Added");
+    }
 }
